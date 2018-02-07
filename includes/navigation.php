0b4ff89af3c797c2e6712bfd449fc14078ef1210
Unknown <sophie.candel@etu.unistra.fr>
2018-02-07 10:53:09 +0100

    navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,19 @@
 <nav>
     <ul>
-        <li><a href="">Accueil</a></li>
-        <li><a href="">À propos</a></li>
-        <li><a href="">Rhums</a></li>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="#">À propos</a>
+            <ul>
+                <li><a href="./pages/apropos/histoire.php">Histoire</a></li>
+                <li><a href="./pages/apropos/savoir_faire.php">Savoir faire</a></li>
+                <li><a href="./pages/apropos/faq.php">FAQ</a></li>
+            </ul>
+        </li>
+        <li><a href="./pages/produits/rhums.php">Rhums</a>
+            <ul>
+                <li><a href="./pages/produits/rhums_blancs.php">Rhums blancs</a></li>
+                <li><a href="./pages/produits/rhums_ambrés.php">Rhums ambrés</a></li>
+                <li><a href="./pages/produits/rhums_épicés.php">Rhums épicés</a></li>
+            </ul>
+        </li>
     </ul>
 </nav>
