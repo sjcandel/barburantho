86f1a5f2824bb0dc2c457c7cbbdfe14148a22db8
Antoine <antoine.quatrelivre@gmail.com>
2018-02-10 15:19:25 +0100

    clean up

--- includes/blog.php
+++
@@ -1,9 +0,0 @@
-<section>
-    <article>
-        <img src="">
-        <div>
-            <h3>Titre de l'article</h3>
-            <p>Description..... bla bla bla </p>
-        </div>
-    </article>
-</section>
